952a8411b7ab8f0e4c0356e57de30a714b390539
Stefan Neidig <stefan@rpdev.net>
2026-02-24 13:58:39 +0100

    chore: add click indicator and correct background to menu

--- src/config/game-config.ts
+++ src/config/game-config.ts
@@ -1,6 +1,6 @@
 import { BootScene } from "@scenes/boot/boot-scene";
-import { GameScene } from "@scenes/game/game-scene";
 import { GameOverScene } from "@scenes/game-over/game-over-scene";
+import { GameScene } from "@scenes/game/game-scene";
 import { MainMenuScene } from "@scenes/main-menu/main-menu-scene";
 import { OptionsScene } from "@scenes/options/options-scene";
 import { UIShowcaseScene } from "@scenes/ui-showcase/ui-showcase-scene";
@@ -10,8 +10,8 @@ import RexUIPlugin from "phaser3-rex-plugins/templates/ui/ui-plugin.js";
 // Game configuration following modular principles
 export const createGameConfig = (): Phaser.Types.Core.GameConfig => ({
 	type: Phaser.AUTO,
-	width: 1080,
-	height: 2400,
+	width: 614,
+	height: 1366, //2400
 	parent: "game-container",
 	backgroundColor: "#2d2d2d",
 	physics: {
